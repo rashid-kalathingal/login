a045c55791b7d85548f8aad0f1a2a033bb9710bb
rashid30111998 <kalathingalrashid89@gmail.com>
2023-01-07 17:05:59 +0530

    first commit

---
+++ routes/router.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  if (req.session.user) {
+    res.render("dashboard", { user: req.session.user, name: req.session.name });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+const credential = [
+  {
+    email: "rashid123@gmail.com",
+    password: "abc@123",
+    name: "mohamed rashid k ",
+  },
+  {
+    email: "rashid456@gmail.com",
+    password: "abc@456",
+    name: "Rashi",
+  },
+];
+var errmsg = null;
+
+router.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.redirect("/");
+  } else {
+    res.render("index", { title: "Login System", err_msg: errmsg });
+    errmsg = null;
+  }
+});
+
+//login user
+router.post("/login", (req, res) => {
+  for (secretkey of credential) {
+    if (
+      req.body.email == secretkey.email &&
+      req.body.password == secretkey.password
+    ) {
+      flag = 0;
+      req.session.user = req.body.email;
+      req.session.name = secretkey.name;
+      res.redirect("/login");
+      break;
+    } else {
+      flag = 1;
+    }
+  }
+  if (flag == 1) {
+    errmsg = "Invalid Username or Password";
+    res.redirect("/login");
+  }
+});
+
+//route for logout
+router.get("/logout", (req, res) => {
+  req.session.destroy(function (err) {
+    if (err) {
+      res.send("Error");
+    } else {
+      res.redirect("/login");
+    }
+  });
+});
+
+module.exports = router;
